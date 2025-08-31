2b63876928b1a990a85e5efca02963be9ecf15e3
Erkhembileg Ariunbold <a.erkhembileg9@gmail.com>
2025-08-31 10:22:33 +0900

    new pipeline test 1

--- content.js
+++ content.js
@@ -1,26 +1,26 @@
-(()=> { // displays in youtube page
+(() => {
+  // displays in youtube page
   let youtubePlayer;
   let curentVideo = "";
   let currentBookmarkedVideos = [];
   let v = "";
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "NEW_BOOKMARK") {
-    console.log("Content script received video ID:", message.videoID);
+    if (message.type === "NEW_BOOKMARK") {
+      console.log("Content script received video ID:", message.videoID);
 
-    // Example: show a little UI injection (like a "Bookmark this video" button)
-    let btn = document.getElementById("yt-bookmark-btn");
-    if (!btn) {
-      btn = document.createElement("button");
-      btn.id = "yt-bookmark-btn";
-      btn.textContent = "🔖 Add Reminder";
-      btn.style.position = "absolute";
-      btn.style.top = "100px";
-      btn.style.right = "20px";
-      btn.style.zIndex = 9999;
-      document.body.appendChild(btn);
+      // Example: show a little UI injection (like a "Bookmark this video" button)
+      let btn = document.getElementById("yt-bookmark-btn");
+      if (!btn) {
+        btn = document.createElement("button");
+        btn.id = "yt-bookmark-btn";
+        btn.textContent = "🔖 Add Reminder";
+        btn.style.position = "absolute";
+        btn.style.top = "100px";
+        btn.style.right = "20px";
+        btn.style.zIndex = 9999;
+        document.body.appendChild(btn);
+      }
     }
-  }
-});
-
+  });
 })();
